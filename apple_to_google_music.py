79033ec5247f1c1b2b394e3dcf69a6a87c2342be
loogle18 <sme@jetthoughts.com>
2018-01-30 00:15:31 +0200

    Added base methods and app structure.

---
+++ apple_to_google_music.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser
+from gmusicapi import Mobileclient
+
+
+def _get_args():
+    parser = ArgumentParser()
+    required = parser.add_argument_group("required arguments")
+    required.add_argument("-e", "--email", metavar="\b", required=True,
+                          help="Google Play Music email.")
+    required.add_argument("-p", "--password", metavar="\b", required=True,
+                          help="Generated app password. See: https://support.google.com/mail/answer/185833?hl=en")
+    required.add_argument("-f", "--file", metavar="\b", required=True,
+                          help="Path to Apple Music playlist file in txt format.")
+    return parser.parse_args()
+
+
+def _get_songs_count_from_file(path):
+    try:
+        with open(path) as file:
+            return sum(1 for line in file) - 1
+    except Exception as error:
+        print(error)
+
+def main():
+    args = _get_args()
+    am_songs_count = _get_songs_count_from_file(args.file)
+    if am_songs_count:
+        print("Found %d songs in passed file" % am_songs_count)
+        api = Mobileclient()
+        api.login(args.email, args.password, Mobileclient.FROM_MAC_ADDRESS)
+
+
+if __name__ == "__main__":
+    main()
